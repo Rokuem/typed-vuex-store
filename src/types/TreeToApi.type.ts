ba41b75c98d032a3dff2fec5d8ed2037e4eb2ee7
mateus amorim <mateusamorim2@gmail.com>
2020-02-29 08:41:32 -0300

    Support optinional payloads

--- src/types/TreeToApi.type.ts
+++ src/types/TreeToApi.type.ts
@@ -1,9 +1,13 @@
 import { StoreOptions } from 'vuex';
 
 export type TreeToApi<T extends StoreOptions<any>[keyof StoreOptions<any>]> = {
-  [key in keyof T]: T[key] extends (...args: [any, infer A]) => any
-    ? keyof A extends keyof unknown
+  [key in keyof T]: T[key] extends (...args: infer A) => any
+    ? A['length'] extends 1
       ? () => ReturnType<T[key]>
-      : (payload: A) => ReturnType<T[key]>
+      : A extends [any, infer P]
+      ? (payload: P) => ReturnType<T[key]>
+      : (payload?: A[1]) => ReturnType<T[key]>
     : never;
 };
+
+// ReturnType<T[key]>
