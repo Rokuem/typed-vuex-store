3d98d607a825f1e10be3fcf52fafc3b240743866
Mateus Amorim <mateusamorim2@gmail.coms>
2021-11-16 09:06:25 -0300

    Add defineGetters

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,7 @@
-import { ActionTree, MutationTree } from 'vuex';
+import { ActionTree, GetterTree, MutationTree } from "vuex";
 
 export const withState = <S, R = any>() => ({
   defineMutations: <T extends MutationTree<S>>(mutations: T) => mutations,
   defineActions: <T extends ActionTree<S, R>>(actions: T) => actions,
+  defineGetters: <T extends GetterTree<S, R>>(getters: T) => getters,
 });
